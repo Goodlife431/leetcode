c8be47bcf65bd8aa0754456d54c3a980dc99dff9
Goodlife431 <111048769+Goodlife431@users.noreply.github.com>
2023-03-22 07:09:29 +0100

    commit

---
+++ binary.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+
+            if remaining in seen:
+                return [i, seen[remaining]]
+
+            seen[value] = i
+
+            
