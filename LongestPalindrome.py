49e2428ba7f3b35eb52622b92780e7075cd25925
Goodlife <oduwolejohn431@gmail.com>
2023-06-07 07:50:26 +0100

    profit

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -4,19 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        longest = ''
-    
-        def expandFromCenter(s, left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
+        def expandFromCenter(s, right, left):
+            while left <= 0 and right < len(s) and s[right] == s[left]:
                 left -= 1
                 right += 1
-            return s[left+1:right]
-    
+            return[left+1:right]
         for i in range(len(s)):
             palindrome_odd = expandFromCenter(s, i, i)
-            palindrome_even = expandFromCenter(s, i, i+1)
-            
-            longest = max(longest, palindrome_odd, palindrome_even, key=len)
-        
-        return longest
+            palindrome_even = expandFromCenter(s, i , i+1)
+            longest_max = max(palindrome_even, palindrome_odd, key=len)
+        return longest_max
         
