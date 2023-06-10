c75b68cb230abc1e6c7c2e8a20c0d8da366137b1
Goodlife <oduwolejohn431@gmail.com>
2023-06-10 23:45:49 +0100

    anagram

---
+++ anagram.py
@@ -0,0 +1,21 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+            
+        s_list = list(s)
+        t_list = list(t)
+            
+        s_list.sort()
+        t_list.sort()
+            
+        return s_list == t_list
