e6cd218e74817f9b1c4ac6763c99e75373d1ca85
Goodlife <oduwolejohn431@gmail.com>
2023-06-11 22:52:59 +0100

    same tree

--- sameTree.py
+++ sameTree.py
@@ -2,3 +2,23 @@
 
 # Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        
+        # If one of the trees is empty, they are not the same
+        if not p or not q:
+            return False
+        
+        # Check if the values of the current nodes are the same
+        if p.val != q.val:
+            return False
+        
+        # Recursively check the left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
