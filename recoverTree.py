244536b62a651a12e4c957d40e8f5ba5dcf08da9
Goodlife <oduwolejohn431@gmail.com>
2023-06-11 19:55:15 +0100

    recover tree

---
+++ recoverTree.py
@@ -0,0 +1,30 @@
+class Solution:
+    def recoverTree(self, root):
+        # Variables to store the misplaced nodes
+        first, second = None, None
+        # Stack for iterative inorder traversal
+        stack = []
+        # Previous node during inorder traversal
+        prev = None
+
+        # Perform iterative inorder traversal
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+
+            # Check if current node is misplaced
+            if prev and prev.val > root.val:
+                if not first:
+                    first = prev
+                second = root
+
+            prev = root
+            root = root.right
+
+        # Swap the values of the misplaced nodes
+        first.val, second.val = second.val, first.val
+
+        return first, second
