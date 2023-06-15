c852d8acb742bf37c268cafa6e72c3a9b550c8cf
Goodlife <oduwolejohn431@gmail.com>
2023-06-15 12:56:04 +0100

    remove duplicate from sorted array

---
+++ removeDuplicateFromSortedList.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        slow = 0
+        fast = 1
+
+        while fast < len(nums):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1
