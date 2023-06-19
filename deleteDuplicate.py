2e898d7274d1576aadb8d95d6fb64a9716aa6b80
Goodlife <oduwolejohn431@gmail.com>
2023-06-19 23:30:48 +0100

    delete duplicate number in a node

---
+++ deleteDuplicate.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+            
+        return head
