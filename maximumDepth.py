b0302219d398fb11a813a9498d0f8f15ed245fb0
Goodlife <oduwolejohn431@gmail.com>
2023-06-21 07:12:48 +0100

    maximum depth

---
+++ maximumDepth.py
@@ -0,0 +1,8 @@
+class solution(object):
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        return max(left_depth, right_depth) + 1 
