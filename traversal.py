cadfd0efadf4db44c2319211ea1d2b91a15ceb72
Goodlife <oduwolejohn431@gmail.com>
2023-06-26 03:46:56 +0100

    traverse

--- traversal.py
+++ traversal.py
@@ -1,3 +1,5 @@
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
