23e309ec817a33608ce3712d68bb87af44a5ad9c
Goodlife <oduwolejohn431@gmail.com>
2023-06-27 23:59:00 +0100

    postordertraverse

---
+++ postOrderTraversal.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        if root:
+            result.extend(self.postorderTraversal(root.left))
+            result.extend(self.postorderTraversal(root.right))
+            result.append(root.val)
+        return result
