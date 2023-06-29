24b31fb026ac4d6baa52a7095dc145ba3e9741b8
Goodlife <oduwolejohn431@gmail.com>
2023-06-29 15:15:53 +0100

    absolute minimum

--- absoluteMinimum.py
+++ absoluteMinimum.py
@@ -1,3 +1,5 @@
+# Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
 class solution(object):
     def getMinimumDifference(self, root):
         stack = []
