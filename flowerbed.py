7b036812c6e6b9bf8071ef09442a257523846676
Goodlife <oduwolejohn431@gmail.com>
2023-06-30 22:54:12 +0100

    flowerbed

--- flowerbed.py
+++ flowerbed.py
@@ -1,3 +1,16 @@
 class solution(object):
     def can_palace_flowers(self, flowerbed, n):
+        flowerbed = [0] + flowerbed + [0]
+        count = 0
+        i = 1
+
+        while i < len(flowerbed) - 1:
+            if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                count += 1
+                i += 2
+            else:
+                i += 1
+        return count >= n
         
+
