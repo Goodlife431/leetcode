a1a721ac35d7facaff441fe884f88379e893e6a4
Goodlife <oduwolejohn431@gmail.com>
2023-07-07 23:42:36 +0100

    straight line

--- striaghtLine.py
+++ striaghtLine.py
@@ -1,13 +1,15 @@
-class solution(object):
-    def checkStraightLine(coordinates):
+class Solution(object):
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
         x0, y0 = coordinates[0]
         x1, y1 = coordinates[1]
-        
-        slope = (y1 - y0) / (x1 -x0)
 
         for i in range(2, len(coordinates)):
             x, y = coordinates[i]
-            if(y -y0) / (x - x0) != slope:
+            if (x - x0) * (y1 - y0) != (x1 - x0) * (y - y0):
                 return False
-            
+
         return True
